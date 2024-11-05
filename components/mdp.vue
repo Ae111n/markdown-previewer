703db6631971db641cd8cb22ea7253687b34e588
Ae111n <bouguedraaymen@gmail.com>
2024-11-05 14:55:26 +0100

    set up the page structure..

--- components/mdp.vue
+++ components/mdp.vue
@@ -2,14 +2,28 @@
   <div id="main">
 
     <h1>fcc markdown previewer</h1>
-    <textarea  id="input"></textarea>
-    <div id="output"></div>
+    <textarea  id="editor" v-model="input"></textarea>
+    <test>{{ input }}</test>
+    <button>parse</button>
+    <div id="preview"></div>
 
   </div>
 </template>
 
 <script>
+import marked from 'marked'
 export default {
+data() {
+    return {
+        input :"#title"
+    }
+},
+computed : {
+ parse() {
+    marked(this.input)
+ }
+
+}
 
 }
 </script>
@@ -28,13 +42,13 @@ display: flex;
 
   
 }
-#input {
+#editor {
  min-width:70%;
    max-width: fit-content ;   
     min-height:90px;
 
 }
-#output {
+#preview {
     min-width:70%;
    max-width: fit-content ;   
     min-height:90px;
